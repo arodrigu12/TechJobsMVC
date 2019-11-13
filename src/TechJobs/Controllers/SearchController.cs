fa428963cfa6bf20df31459b12c026539af8406c
Alfredo Rodriguez <arodrigu12@hotmail.com>
2019-11-13 16:50:47 -0500

    Includes all Bonus Missions, except partial views and 'the big one'

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -15,6 +16,32 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (String.IsNullOrWhiteSpace(searchTerm))
+	        {
+                ViewBag.errorMsg = "Please enter a search term.";
+                ViewBag.columns = ListController.columnChoices;
+                return View("Index");
+	        }   
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            if (searchType.Equals("all"))
+            {
+                jobs = JobData.FindByValue(searchTerm);
+
+            }
+            else 
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.jobs = jobs;
+            ViewBag.searchTerm = searchTerm;
+            return View("Index");
+        }
 
     }
 }
