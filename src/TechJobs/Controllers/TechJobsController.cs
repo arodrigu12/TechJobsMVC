c947800eda6400d18d33ac965107d39a4c470e4e
Alfredo Rodriguez <arodrigu12@hotmail.com>
2019-11-14 09:46:45 -0500

    Completed TechJobsMVC, including all Bonus Missions, except partial views.

---
+++ src/TechJobs/Controllers/TechJobsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechJobs.Controllers
+{
+    public class TechJobsController : Controller
+    {
+        internal static Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+
+        internal static Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+
+        static TechJobsController()
+        {
+            //Initialize actionChoices
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
+
+            //Initialize columnChoices
+            columnChoices.Add("core competency", "Skill");
+            columnChoices.Add("employer", "Employer");
+            columnChoices.Add("location", "Location");
+            columnChoices.Add("position type", "Position Type");
+            columnChoices.Add("all", "All");
+        }
+
+        public override ViewResult View()
+        {
+            ViewBag.actions = actionChoices;
+            ViewBag.columns = columnChoices;
+
+            return base.View();
+        }
+
+        public override ViewResult View(string viewName)
+        {
+            ViewBag.actions = actionChoices;
+            ViewBag.columns = columnChoices;
+
+            return base.View(viewName);
+        }
+    }
+}
